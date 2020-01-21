c9f8466fbf4247ca89717b7a8ad47ca4abf7f873
cahc <cherrerac@gmail.com>
2020-01-21 16:44:10 -0500

    crud inventario funcionando

--- src/main/java/com/cahc/proyecto/controller/InventarioController.java
+++ src/main/java/com/cahc/proyecto/controller/InventarioController.java
@@ -2,6 +2,8 @@ package com.cahc.proyecto.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,13 +28,39 @@ public class InventarioController {
 		return model;		
 	}
 
+	@RequestMapping(value="/addInventario", method=RequestMethod.GET)
+	public ModelAndView addInventario() {
+		ModelAndView model = new ModelAndView();
+		
+		Inventario inventario = new Inventario();
+		model.addObject("inventarioForm", inventario);
+		model.setViewName("inventario_form");
+		
+		return model;
+	}
 
-@RequestMapping(value="/list", method=RequestMethod.GET)
-public ModelAndView list() {
-	ModelAndView model = new ModelAndView("inventario_list");
-	List<Inventario> inventarioList = inventarioService.getAllInventarios();
-	model.addObject("inventarioList", inventarioList);
-	
-	return model;
-}
+	@RequestMapping(value="/updateInventario/{id}", method=RequestMethod.GET)
+	public ModelAndView editInventario(@PathVariable long id) {
+		ModelAndView model =new ModelAndView();
+		
+		Inventario inventario = inventarioService.getInventarioById(id);
+		model.addObject("inventarioForm", inventario);
+		model.setViewName("inventario_form");
+		
+		return model;
+	}
+
+	@RequestMapping(value="/saveInventario", method=RequestMethod.POST)
+	public ModelAndView save(@ModelAttribute("inventarioForm") Inventario inventario) {
+		inventarioService.saveOrUpdate(inventario);
+		
+		return new ModelAndView("redirect:/inventario/listar");
+	}
+
+	@RequestMapping(value="/deleteInventario/{id}", method=RequestMethod.GET)
+	public ModelAndView delete(@PathVariable("id") long id) {
+		inventarioService.deleteInventario(id);
+		
+		return new ModelAndView("redirect:/inventario/listar");
+	}
 }
